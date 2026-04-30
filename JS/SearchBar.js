1cefd3ac24ad669d4d50ddc09cce42b2685c8605
Level-X <84361339+Level-X@users.noreply.github.com>
2026-04-29 20:55:46 -0500

    Updated for v5

--- JS/SearchBar.js
+++ JS/SearchBar.js
@@ -6,7 +6,7 @@
    enabling keyboard navigation which is good for accessibility. It would
    be very tedious to add an incrementing "tabindex" attribute on every
    image by hand, though, and impossible to update it when you added a
-   new Pokémon in the middle! So let's add them with javascript. */ 
+   new Pokémon in the middle! So let's add them with javascript. */
 function addTabIndex() {
   const targets = Array.from(document.querySelectorAll('main img:not([src="Images/Pokémon/0.png"])'));
   // Get the list of all non-placeholder Pokémon, convert it to an array.
@@ -22,14 +22,14 @@ function addSearch() {
   // Create a new div element:
   const searchHead = document.createElement("div");
   // Style it with a class:
-  searchHead.classList.add("head");
+  searchHead.classList.add("title");
   // And give it some content:
   const searchHeadText = document.createTextNode("Jump to a Pokémon");
   // Add the text node to the newly created div:
   searchHead.appendChild(searchHeadText);
   // Add the newly created element and its content into the DOM:
   const theNav = document.getElementsByTagName("nav")[0]; // assume only one nav, else we have to put an ID on the nav
-  theNav.append(searchHead);
+
   // Repeat!
   const searchBox = document.createElement("div");
   searchBox.classList.add("search");
@@ -50,7 +50,9 @@ function addSearch() {
   searchBox.append(searchBoxBox);
   searchBox.append(searchBoxButton);
   searchBox.append(searchBoxText);
-  theNav.append(searchBox);
+  const titles = theNav.querySelectorAll("div.title");
+  theNav.insertBefore(searchHead, titles[1]);
+  theNav.insertBefore(searchBox, titles[1]);
   // Add an anonymous function to the button's onclick attribute:
   searchBoxButton.onclick = () => {
     let uSearchValue = document.getElementById("searchText").value
@@ -60,24 +62,24 @@ function addSearch() {
     // Get the list of all non-placeholder Pokémon, convert it to an array.
     let searchTarget = searchTargets.find((img) => img.alt.normalize("NFD").replace(/\p{Diacritic}/gu, "").toLowerCase().startsWith(searchValue));
     // The first img in searchTargets where img.alt.normalize("NFD").replace(/\p{Diacritic}/gu, "").toLowerCase() === searchValue.
-    if ( searchTarget != null ) { 
+    if (searchTarget != null) {
       searchTarget.focus({ preventScroll: true });
       /* Focus the searchTarget to highlight it as the search result.
       Don't scroll to it, because we want to do a nicer scroll ourselves with: */
       searchTarget.scrollIntoView({ behavior: "smooth", block: "center" });
       searchBoxText.innerText = "Click or press Enter to mark the Pokémon as owned!"
-      searchBoxText.style = "display: initial; color: #fff;";
+      searchBoxText.style = "display: initial; color: #247000;";
     } else {
       searchBoxText.innerText = "Couldn't find a name starting with '" + uSearchValue + "'!";
-      searchBoxText.style = "display: initial; color: #ff7c7c;";
+      searchBoxText.style = "display: initial; color: #670000;";
     }
   }
   // Add an event listener for the Enter key to the search box:
-  searchBoxBox.addEventListener("keyup", function(event) {
+  searchBoxBox.addEventListener("keyup", function (event) {
     event.preventDefault();
     if (event.key === "Enter") {
       // if we get it, click the Search button
-        searchBoxButton.click();
+      searchBoxButton.click();
     }
   });
 }
